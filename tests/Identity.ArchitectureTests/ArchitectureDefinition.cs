c56025036f5e8654312216533ea50587bfb8d45d
SamuelSolorzano20 <samuelsolorzanojp@gmail.com>
2026-07-22 19:42:38 -0600

    ArchUnitNET installed & architecture definiton created

---
+++ tests/Identity.ArchitectureTests/ArchitectureDefinition.cs
@@ -0,0 +1,15 @@
+using ArchUnitNET.Domain;
+using ArchUnitNET.Loader;
+
+namespace Identity.ArchitectureTests;
+
+internal static class ArchitectureDefinition 
+{
+    public static readonly Architecture Architecture = new ArchLoader()
+        .LoadAssemblies(
+            typeof(Domain.AssemblyReference).Assembly,
+            typeof(Application.AssemblyReference).Assembly,
+            typeof(Infrastructure.AssemblyReference).Assembly,
+            typeof(Api.AssemblyReference).Assembly
+        ).Build();
+}
